8414ef820852153990f72539437271706c19e186
小嘿 <@AiYi>
2019-11-16 09:30:39 +0800

    my更新了user

--- StudentManagerSystem/src/main/java/cn/edu/xcu/controller/UserController.java
+++ StudentManagerSystem/src/main/java/cn/edu/xcu/controller/UserController.java
@@ -2,8 +2,16 @@ package cn.edu.xcu.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
+import cn.edu.xcu.entity.User;
+import cn.edu.xcu.service.IUserService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * <p>
@@ -13,9 +21,53 @@ import org.springframework.stereotype.Controller;
  * @author dcn
  * @since 2019-11-15
  */
-@Controller
-@RequestMapping("/user")
+@RestController
 public class UserController {
-
+	@Autowired
+	private IUserService iUserService;
+	@RequestMapping("/add")
+	public String UserAdd(User user) {
+		boolean ret=iUserService.save(user);
+		if(ret) {
+			return "/main";
+		}else {
+	        return "redirect:/add";	
+		}
+	
+	}
+	
+	@RequestMapping("/del")
+	public Map<String,Object> delete(Integer id){
+		boolean result=iUserService.removeById(id);
+		Map<String,Object> ret=new HashMap<>();
+		if(result){
+			ret.put("code",1);
+			ret.put("msg","删除成功");
+			}else{
+				ret.put("code",-1);
+				ret.put("msg","删除失败");
+				}
+		return ret;
+		}
+	@RequestMapping("/update")
+	public Map<String,Object> update(User user){
+		boolean result=iUserService.updateById(user);
+		Map<String,Object> ret=new HashMap<>();
+		if(result){
+			ret.put("code",1);
+			ret.put("msg","编辑成功");
+			}else{
+				ret.put("code",-1);
+				ret.put("msg","编辑失败");
+		}
+		return ret;
+		
+	}
+	
+	
+	@RequestMapping("/findAll")
+	public List<User> findAll(){
+		return iUserService.list();
+	}
 }
 
