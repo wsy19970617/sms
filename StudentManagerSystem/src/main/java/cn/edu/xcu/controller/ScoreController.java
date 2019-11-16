248bd2d41810cb223df03706b91118096b465cde
pc <pc@DESKTOP-GPVBOV2>
2019-11-16 09:25:45 +0800

    xk也随便操作了一下

--- StudentManagerSystem/src/main/java/cn/edu/xcu/controller/ScoreController.java
+++ StudentManagerSystem/src/main/java/cn/edu/xcu/controller/ScoreController.java
@@ -1,9 +1,21 @@
 package cn.edu.xcu.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import cn.edu.xcu.entity.Score;
+import cn.edu.xcu.service.IScoreService;
 
 /**
  * <p>
